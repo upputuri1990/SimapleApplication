f0bf2e1b473fb6a4f55897f01cc5a0b99a2e8404
naveen <naveen@10.0.0.154>
2016-07-22 15:12:10 -0700

    Adding a addition confirmation message
    Change-Id: Ie67f323d4407d280afed746d40a27e68b39f7b2d
    Signed-off-by: naveen <naveen@10.0.0.154>

--- SimapleApplication/src/domain/Main.java
+++ SimapleApplication/src/domain/Main.java
@@ -2,6 +2,7 @@ package domain;
 
 public class Main {
 	public static void main(String[] args) {
+		System.out.println("Addition completed successfully");
 		Addition addition=new Addition();
 		addition.addition();
 	}
