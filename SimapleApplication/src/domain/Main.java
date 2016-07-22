27ae5c2dc4206072b8118b74cb16b66bf74d9731
naveen <naveen@10.0.0.154>
2016-07-22 15:53:15 -0700

    specifying next mathematical operation
    Change-Id: I08cc299431e3fc9692d8b9a03b61c2c9d0b90386
    Signed-off-by: naveen <naveen@10.0.0.154>

--- SimapleApplication/src/domain/Main.java
+++ SimapleApplication/src/domain/Main.java
@@ -3,8 +3,10 @@ package domain;
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("Addition completed successfully");
+		
 		Addition addition=new Addition();
 		addition.addition();
+		System.out.println("next i want to do substraction ");
 	}
 
 }
