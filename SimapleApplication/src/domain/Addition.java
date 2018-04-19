8328f61c76df72fa7bab7e5e60f2d7c7590e8d70
upputuri1990 <upputuri1990@gmail.com>
2018-04-18 23:41:18 -0500

    modifying 4 number sum

--- SimapleApplication/src/domain/Addition.java
+++ SimapleApplication/src/domain/Addition.java
@@ -1,14 +1,14 @@
 package domain;
 
 public class Addition {
-	int a=10,b=15,c=25;
+	int a=10,b=15,c=25, d=45;
 	public void addition(){
-		int d=a + b +c;
+		int e=a + b +c +d;
 		System.out.println("Addition of two numbers is"+c);
 	}
 	public void substraction()
 	{
-		int d=a - b;
+		int f=a - b;
 		System.out.println("substraction of two numbers is"+d);
 		System.out.println("Next i want to do multiplication ");
 	}
