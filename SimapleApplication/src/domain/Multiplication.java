1690d28d8cb138dee25b08138ec2f929e814d274
upputuri1990 <upputuri1990@gmail.com>
2018-04-18 23:56:37 -0500

    multiplication of three numbers

--- SimapleApplication/src/domain/Multiplication.java
+++ SimapleApplication/src/domain/Multiplication.java
@@ -4,8 +4,9 @@ public class Multiplication {
 	public static void main(String[] args) {
 		int a=4;
 		int b=10;
-		int c=a * b;
-		System.out.println( c);
+		int c=100;
+		int d=a * b * c;
+		System.out.println( d);
 		
 	}
 
